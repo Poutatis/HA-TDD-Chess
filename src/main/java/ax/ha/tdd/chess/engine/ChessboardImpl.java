73accce503c4297d0443de1d645b4e5a68a513b3
Poutatis <97979895+Poutatis@users.noreply.github.com>
2024-05-02 23:27:18 +0300

    Update ChessboardImpl.java
    fixat detta Error:  ax.ha.tdd.chess.engine.ChessboardTests.testStartingBoardQueensInTheRightSpot -- Time elapsed: 0.012 s <<< FAILURE!
    org.opentest4j.AssertionFailedError: expected: <BLACK ChessPieceStub> but was: <BLACK Queen>

--- src/main/java/ax/ha/tdd/chess/engine/ChessboardImpl.java
+++ src/main/java/ax/ha/tdd/chess/engine/ChessboardImpl.java
@@ -15,12 +15,12 @@ public class ChessboardImpl implements Chessboard {
     public static ChessboardImpl startingBoard() {
         final ChessboardImpl chessboard = new ChessboardImpl();
 
-        chessboard.withMirroredPiece(PieceType.PAWN, List.of(0, 1, 2, 3, 4, 5, 6, 7), 1)
-                .withMirroredPiece(PieceType.ROOK, List.of(0, 7), 0)
-                .withMirroredPiece(PieceType.KNIGHT, List.of(1, 6), 0)
-                .withMirroredPiece(PieceType.BISHOP, List.of(2, 5), 0)
-                .withMirroredPiece(PieceType.QUEEN, List.of(3), 0)
-                .withMirroredPiece(PieceType.KING, List.of(4), 0);
+        chessboard.withMirroredPieceStub(PieceType.PAWN, List.of(0, 1, 2, 3, 4, 5, 6, 7), 1)
+                .withMirroredPieceStub(PieceType.ROOK, List.of(0, 7), 0)
+                .withMirroredPieceStub(PieceType.KNIGHT, List.of(1, 6), 0)
+                .withMirroredPieceStub(PieceType.BISHOP, List.of(2, 5), 0)
+                .withMirroredPieceStub(PieceType.QUEEN, List.of(3), 0)
+                .withMirroredPieceStub(PieceType.KING, List.of(4), 0);
         return chessboard;
     }
 
@@ -47,36 +47,11 @@ public class ChessboardImpl implements Chessboard {
      * @param yCoordinate  yCoordinateOffset
      * @return itself, like a builder pattern
      */
-    private ChessboardImpl withMirroredPiece(final PieceType pieceType,
-                                             final List<Integer> xCoordinates, final int yCoordinate) {
+    private ChessboardImpl withMirroredPieceStub(final PieceType pieceType,
+                                                 final List<Integer> xCoordinates, final int yCoordinate) {
         xCoordinates.forEach(xCoordinate -> {
-            switch (pieceType) {
-                case PAWN -> {
-                    addPiece(new Pawn(Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new Pawn(Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-                case ROOK -> {
-                    addPiece(new Rook(Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new Rook(Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-                case KNIGHT -> {
-                    addPiece(new Knight(Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new Knight(Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-                case BISHOP -> {
-                    addPiece(new Bishop(Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new Bishop(Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-                case QUEEN -> {
-                    addPiece(new Queen(Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new Queen(Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-                //TODO, when you implement a piece, add it as a case in this switch
-                default -> {
-                    addPiece(new ChessPieceStub(pieceType, Color.BLACK, new Square(xCoordinate, yCoordinate)));
-                    addPiece(new ChessPieceStub(pieceType, Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
-                }
-            }
+            addPiece(new ChessPieceStub(pieceType, Color.BLACK, new Square(xCoordinate, yCoordinate)));
+            addPiece(new ChessPieceStub(pieceType, Color.WHITE, new Square(xCoordinate, 7 - yCoordinate)));
         });
         return this;
     }
